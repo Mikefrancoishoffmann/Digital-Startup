621045836a7de8107e6ddccacf626c2904120cbb
Mikefrancoishoffmann <48518432+Mikefrancoishoffmann@users.noreply.github.com>
2020-05-08 11:16:17 +0200

    Add files via upload

---
+++ dist/precache-manifest.f814bd64069e66b02caf490e9d4ee5a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9bf67bcb860870e6b0",
+    "url": "/css/app.5c9713c3.css"
+  },
+  {
+    "revision": "5bc312d7414263005448371bf5ca9db3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c01562fd9d4b2ceb860",
+    "url": "/js/about.c2760050.js"
+  },
+  {
+    "revision": "0f9bf67bcb860870e6b0",
+    "url": "/js/app.1349bd8b.js"
+  },
+  {
+    "revision": "21677021b198814068f2",
+    "url": "/js/chunk-vendors.c27cf46e.js"
+  },
+  {
+    "revision": "f7b9ce85d5c4165ba7bd9ea17f006a27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
